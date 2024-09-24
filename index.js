eeb0f99de04ac273268c2dcf6b878c393e9a6889
EngenMe <mohamed_farouk.hasnaoui@g.enp.edu.dz>
2024-09-24 03:36:43 +0100

    Authenticate with jwt.

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ const Fawn = require('fawn');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const users = require('./routes/users');
+const auth = require('./routes/auth');
+const config = require('config');
+
+if (!config.get('jwtPrivateKey')) {
+  console.error('FATAL ERROR: jwtPrivateKey is not defined!');
+  process.exit(1);
+}
 
 mongoose
   .connect('mongodb://localhost/vidly')
@@ -24,6 +31,7 @@ app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
---
+++ routes/auth.js
@@ -0,0 +1,42 @@
+const { User } = require('../models/user');
+const express = require('express');
+const router = express.Router();
+const _ = require('lodash');
+const bcrypt = require('bcrypt');
+const Joi = require('joi');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+function validate(req) {
+  const schema = Joi.object({
+    email: Joi.string()
+      .email({ tlds: { allow: true } })
+      .min(5)
+      .max(255)
+      .trim()
+      .required(),
+    password: Joi.string().required()
+  });
+
+  return schema.validate(req);
+}
+
+router.post('/', async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send('Invalid email or password!');
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).send('Invalid email or password!');
+
+  const newUser = new User(_.pick(req.body, ['name', 'email', 'password']));
+  const salt = await bcrypt.genSalt(10);
+  newUser.password = await bcrypt.hash(newUser.password, salt);
+
+  const token = jwt.sign({ _id: user._id }, config.get('jwtPrivateKey'));
+  res.send(token);
+});
+
+module.exports = router;
