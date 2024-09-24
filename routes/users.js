7d512e73742d83e463c4a4bf569877f8845c714f
EngenMe <mohamed_farouk.hasnaoui@g.enp.edu.dz>
2024-09-24 05:33:24 +0100

    Add get current user endpoint.

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,12 @@ const _ = require('lodash');
 const bcrypt = require('bcrypt');
 const auth = require('../middlewares/auth');
 
-router.post('/', auth, async (req, res) => {
+router.get('/me', auth, async (req, res) => {
+  const user = await User.findById(req.user._id).select('-password');
+  res.send(user);
+});
+
+router.post('/', async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
